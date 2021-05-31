94fda61b9ce087c00ca30e13271c4c86020a7281
Dalmeida Jean-eudes <Jdalmeida2908@gmail.com>
2021-05-31 11:18:14 +0000

    assignment

--- src/Room.java
+++ src/Room.java
@@ -1,9 +1,37 @@
 
 public class Room {
+	static int roomno;
+	static String roomtype;
+	static int roomarea;
+	static String Acmachine;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public static int displaydataRoomno() {
+		return roomno;
+	}
+	public static void setdataRoomno(int roomno) {
+		Room.roomno = roomno;
+	}
+	public static String displaydataRoomtype() {
+		return roomtype;
+	}
+	public static void setdataRoomtype(String roomtype) {
+		Room.roomtype = roomtype;
+	}
+	public static int displaydataRoomarea() {
+		return roomarea;
+	}
+	public static void setdataRoomarea(int roomarea) {
+		Room.roomarea = roomarea;
+	}
+	public static String displaydataAcmachine() {
+		return Acmachine;
+	}
+	public static void setdataAcmachine(String acmachine) {
+		Acmachine = acmachine;
 	}
 
 }
